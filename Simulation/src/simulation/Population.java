742b23c44c40dc7303aecfe554d1e5c6b5e71837
Gerdons <Mr.rotschafer@gmail.com>
2017-04-10 20:39:44 -0500

    Things

--- Simulation/src/simulation/Population.java
+++ Simulation/src/simulation/Population.java
@@ -6,19 +6,26 @@ public class Population
 {
 	private ArrayList<Member> MemList; // An array of all members in the population.
 	private ArrayList<Tile> OwnedTiles; // An array of all Owned Tiles of the population.
-		
+	
+	private float popRate;	
 	private static int  currentpersonindex = 0;
+
+	public float getPopRate() {
+		return popRate;
+	}
 	
 	public Tile UpdateTile(Tile tile){
 		
 		return tile; 
 	}
-	public Tile AddTile(Tile tile){
-		
-		return tile; 
+	public Tile AddTile(){
+		return null; 
 	}
 	public void RemoveTile(Tile tile){
 		
 	 
 	}
+
+	
+	
 }
