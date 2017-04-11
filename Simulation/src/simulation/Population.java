7946c8cf4c446bb37ca6d1d5cc474d286d2f8078
Gerdons <Mr.rotschafer@gmail.com>
2017-04-10 20:19:56 -0500

    Updated Fields and method calls.

--- Simulation/src/simulation/Population.java
+++ Simulation/src/simulation/Population.java
@@ -1,6 +1,24 @@
 package simulation;
 
+import java.util.ArrayList;
+
 public class Population
 {
-
+	private ArrayList<Member> MemList; // An array of all members in the population.
+	private ArrayList<Tile> OwnedTiles; // An array of all Owned Tiles of the population.
+		
+	private static int  currentpersonindex = 0;
+	
+	public Tile UpdateTile(Tile tile){
+		
+		return tile; 
+	}
+	public Tile AddTile(Tile tile){
+		
+		return tile; 
+	}
+	public void RemoveTile(Tile tile){
+		
+	 
+	}
 }
